7752193d2533037ef344baec262d1d0fd5749ce8
Victor Peres <victor.peres93@gmail.com>
2017-10-10 23:41:10 -0300

    reading access token

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import json
 
 with open('secret.json') as f:
 	secret = json.load(f)
+with open('token.json') as f2:
+	access = json.load(f2)
 
-print (secret["key"])
+api = twitter.Api(consumer_key=secret['key'],
+	consumer_secret=secret['secret'],
+	access_token_key=access['key'],
+	access_token_secret=access['secret'])
