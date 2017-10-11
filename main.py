230aeb7d5d3771b5f8b26f20f6e8e9335d161fa0
Victor Peres <victor.peres93@gmail.com>
2017-10-10 23:44:43 -0300

    putting in a function

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import twitter
 import json
 
-with open('secret.json') as f:
-	secret = json.load(f)
-with open('token.json') as f2:
-	access = json.load(f2)
+def load_api():
+	with open('secret.json') as f:
+		secret = json.load(f)
+	with open('token.json') as f2:
+		access = json.load(f2)
 
-api = twitter.Api(consumer_key=secret['key'],
-	consumer_secret=secret['secret'],
-	access_token_key=access['key'],
-	access_token_secret=access['secret'])
+	api = twitter.Api(consumer_key=secret['key'],
+		consumer_secret=secret['secret'],
+		access_token_key=access['key'],
+		access_token_secret=access['secret'])
+	return api
+
+api = load_api()
