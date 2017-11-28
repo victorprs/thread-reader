d5ab52b3653bac9c992d22c9ee179ca2df68a555
Victor Peres <victor.peres93@gmail.com>
2017-11-28 13:23:01 -0200

    formatting html text

--- main.py
+++ main.py
@@ -17,13 +17,13 @@ api = load_api()
 
 def replaceUrl(status):
 	for url_obj in status.urls:
-		status.full_text = status.full_text.replace(url_obj.url, url_obj.expanded_url)
+		status.full_text = status.full_text.replace(url_obj.url, '<a href="' + url_obj.expanded_url + '">' + url_obj.expanded_url + '</a>')
 	return status.full_text
 
 def replaceMedia(status):
 	for media_obj in status.media:
 		if (media_obj.type == 'photo'):
-			status.full_text = status.full_text.replace(media_obj.url, media_obj.expanded_url)
+			status.full_text = status.full_text.replace(media_obj.url, '<p><img src="' + media_obj.media_url_https + '"></p>')
 		elif (media_obj.type == 'video'):
 			bitrate = 0
 			video_url = ''
@@ -33,21 +33,31 @@ def replaceMedia(status):
 						video_url = video_variant['url']
 				except KeyError as e:
 					continue
-			status.full_text = status.full_text.replace(media_obj.url, video_url)
+			status.full_text = status.full_text.replace(media_obj.url, '<p><video controls=""><source src="' + video_url + '"></video></p>')
 	return status.full_text
+
+def formatStatus(status):
+	status_str = ''
+	if (len(status.urls) != 0):
+		status_str = status_str + replaceUrl(status)
+	if (status.media != None):
+		status_str = status_str + replaceMedia(status)
+	if (status.media == None and len(status.urls) == 0):
+		status_str = status_str + status.full_text
+	return '</p>' + status_str + '</p>'
+
 
 def getThreadContent(last_id):
 	statuses = []
 	status = api.GetStatus(last_id)
-	statuses.append(status.full_text)
+	statuses.append(formatStatus(status))
 	i = 0
 	while (status.in_reply_to_status_id != None):
-		print(status)
 		if i > 10: break
 		status = api.GetStatus(status.in_reply_to_status_id)
-		statuses.append(status)
+		statuses.append(formatStatus(status))
 		i = i + 1
-
-
-# status = replaceMedia(status)
-# print(status.full_text)
+	html_str = ''
+	for status_str in reversed(statuses):
+		html_str = html_str + status_str
+	return html_str
